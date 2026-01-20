66cf4cea05128d8ef0db27b0cc5e53ac50618ebe
mohamedghonaim000 <mohamedmokhtar64232@gmail.com>
2026-01-20 10:19:10 +0200

    feat(ui): add navbar and footer components

---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-1 flex items-center gap-6">
+          <a className="btn btn-ghost text-xl">Movies</a>
+
+          <ul className="menu menu-horizontal px-1 hidden md:flex">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex gap-2">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="User avatar"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+
+            <ul
+              tabIndex={-1}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
